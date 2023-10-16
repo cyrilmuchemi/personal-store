f7a0eec33b313bb31b857bd8d43d5a9557708738
cyrilmuchemi <cyril.muchemi@gmail.com>
2023-10-16 13:23:20 +0300

    Create a basic php router

--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo "home page";
+define('ROOT', 'http://localhost/personal-store/public');
+
+$url = $_GET['url'] ?? 'home';
+$url = explode('/', $url);
+
+$page_name = trim($url[0]);
+
+$file_name = "../app/pages/".$page_name.".php";
+
+if(file_exists($file_name))
+{
+    require_once $file_name;
+} else
+{
+    require_once "../app/pages/404.php";
+}
