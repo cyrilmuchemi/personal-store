a6c6ad28e53b41e9b0e78b4d1a93b5b58c0eb6ee
cyrilmuchemi <cyril.muchemi@gmail.com>
2023-10-18 17:08:53 +0300

    Complete signup && login forms

--- app/pages/login.php
+++ app/pages/login.php
@@ -17,30 +17,35 @@
         include '../app/pages/includes/header.php';
     ?>
     <main>
-        <div class="container">
-            <div class="login-box d-flex justify-content-center my-5">
+    <div class="container">
+            <div class="login-box d-flex my-5">
                 <div class="login-bg mt-5">
-                    <img src="<?=ROOT?>/assets/vectors/login-vector.png" alt="login vector image"/>
+                    <img src="<?=ROOT?>/assets/vectors/login-vector.png" alt="signup vector image"/>
                 </div>
-                <div class="login-form">
-                    <h2 class="text-center font-oswold">Sign In Your Account</h2>
+                <div class="login-container">
+                    <h2 class="text-center font-oswold pt-4">Sign In to Your Account</h2>
+                    <div class="login-form">
                     <form class="mt-5" action="" method="post">
-                        <label>Username</label>
-                        <div class="my-2">
-                            <input class="form-input" type="text" placeholder="User Name" name="username" required>
+                        <div class="my-2 d-flex signin-input">
+                           <div>
+                           <label class="label">Username</label>
+                           <input class="form-input" type="text" placeholder="Username" name="username" required>
+                           </div>
                         </div>
-                        <label>Password</label>
-                        <div class="my-2">
-                            <input class="form-input" type="text" placeholder="Password" name="username" required>    
-                        </div> 
-                        <div class="d-flex justify-content-center align-items-center">
-                            <div class="form-check text-start my-3">
+                        <div class="my-2 d-flex signin-input">
+                           <div>
+                           <label class="label">Password</label>
+                           <input class="form-input" type="text" placeholder="Password" name="password" required>
+                           </div>
+                        </div>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="form-check text-start my-3 sign-up-text">
                                 <input name="remember" class="form-check-input" type="checkbox" value="1" id="flexCheckDefault">
                                 <label class="form-check-label" for="flexCheckDefault">
                                     Remember me
                                 </label>
                             </div>
-                            <div class="px-4 text-green">Forgot Your Password?</div>
+                            <div class="px-4 text-green sign-up-text">Forgot Your Password?</div>
                         </div>
                         <div class="my-2">
                             <button class="btn-view btn-border-pop btn-background-slide uppercase">
@@ -51,9 +56,11 @@
                         </button>
                         </div>
                         <div class="my-4">
-                            <h5>Don't have an account? <span class="text-green">Create Account</span></h5>
+                            <h5>Don't have an account? <span class="text-green">Create</span></h5>
                         </div>
                     </form>
+                    <div class="alert alert-info" style="display: none;"></div>
+                    </div>
                 </div>
             <div>
         </div>
--- app/pages/signup.php
+++ app/pages/signup.php
@@ -10,6 +10,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500&family=Poppins:ital,wght@0,300;1,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/css/intlTelInput.css"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/intlTelInput.min.js"></script>
     <title>Accounts Zone - Signup</title>
 </head>
 <body class="font-default">
@@ -18,61 +20,60 @@
     ?>
     <main>
         <div class="container">
-            <div class="login-box d-flex justify-content-center my-5">
+            <div class="login-box d-flex my-5">
                 <div class="login-bg mt-5">
                     <img src="<?=ROOT?>/assets/vectors/signup-vector.png" alt="signup vector image"/>
                 </div>
-                <div class="login-form">
-                    <h2 class="text-center font-oswold">Create an Account</h2>
+                <div class="login-container">
+                    <h2 class="text-center font-oswold pt-4">Create an Account</h2>
+                    <div class="login-form">
                     <form class="mt-5" action="" method="post">
-                        <div class="my-2 d-flex">
+                        <div class="my-2 d-flex signin-input">
                            <div>
-                           <label>Username</label>
+                           <label class="label">Username</label>
                            <input class="form-input" type="text" placeholder="Username" name="username" required>
                            </div>
                            <div>
-                           <label>Email</label>
+                           <label class="label">Email</label>
                            <input class="form-input" type="text" placeholder="Email" name="email" required>
                            </div>
                         </div>
-                        <div class="my-2 d-flex">
-                           <label>Phone :</label>
-                           <!--Phone -->
-                           <div class="form-group">
-                                <input type="text" id="mobile_code" class="form-control" placeholder="Phone Number" name="phone">
-                            </div>
+                        <div class="my-2 phone-container">
+                           <label class="label">Phone Number:</label>
+                           <input class="form-input  phone-container" id="phone" type="tel" name="phone" />
                         </div>
-                        <div class="my-2 d-flex">
+                        <div class="my-2 d-flex signin-input">
                            <div>
-                           <label>Password</label>
+                           <label class="label">Password</label>
                            <input class="form-input" type="text" placeholder="Password" name="password" required>
                            </div>
                            <div>
-                           <label>Confirm Pasword</label>
+                           <label class="label">Confirm Pasword</label>
                            <input class="form-input" type="text" placeholder="Confirm Password" name="confirmpassword" required>
                            </div>
                         </div>
-                        <div class="d-flex justify-content-center align-items-center">
-                            <div class="form-check text-start my-3">
+                        <div class="d-flex">
+                            <div class="form-check text-start my-3 sign-up-text">
                                 <input name="remember" class="form-check-input" type="checkbox" value="1" id="flexCheckDefault">
                                 <label class="form-check-label" for="flexCheckDefault">
-                                    Remember me
+                                    Accept Terms and conditions
                                 </label>
                             </div>
-                            <div class="px-4 text-green">Forgot Your Password?</div>
                         </div>
                         <div class="my-2">
                             <button class="btn-view btn-border-pop btn-background-slide uppercase">
-                                Sign In
+                                Sign Up
                             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
                             </svg>
                         </button>
                         </div>
                         <div class="my-4">
-                            <h5>Don't have an account? <span class="text-green">Create Account</span></h5>
+                            <h5>Already have an account? <span class="text-green">Login</span></h5>
                         </div>
                     </form>
+                    <div class="alert alert-info" style="display: none;"></div>
+                    </div>
                 </div>
             <div>
         </div>
@@ -85,5 +86,19 @@
     ?>
     <script src="<?=ROOT?>/assets/js/index.js"></script>
     <script src="<?=ROOT?>/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script>
+        const phoneInputField = document.querySelector("#phone");
+        const phoneInput = window.intlTelInput(phoneInputField, {
+         utilsScript:
+       "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+        });
+        const info = document.querySelector(".alert-info");
+        function process(event) {
+        event.preventDefault();
+        const phoneNumber = phoneInput.getNumber();
+        info.style.display = "";
+        info.innerHTML = `Phone number in E.164 format: <strong>${phoneNumber}</strong>`;
+        }
+    </script>
 </body>
 </html>
