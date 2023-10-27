b2e41172f4fe3a432ca3f1c0eb4d8ca4ca0598e7
cyrilmuchemi <cyril.muchemi@gmail.com>
2023-10-27 16:05:43 +0300

    Delete adin page & start afresh admin

---
+++ app/pages/admin/blogs.php
@@ -0,0 +1,1 @@
+<div>Blogs Page</div>
--- app/pages/admin/categories.php
+++ app/pages/admin/categories.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Users / Profile - NiceAdmin Bootstrap Template</title>
+  <title>Categories - Accounts Zone</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -701,16 +701,6 @@
   </footer><!-- End Footer -->
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Vendor JS Files -->
-  <script src="<?=ROOT?>/assets/vendor/apexcharts/apexcharts.min.js"></script>
-  <script src="<?=ROOT?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="<?=ROOT?>/assets/vendor/chart.js/chart.umd.js"></script>
-  <script src="<?=ROOT?>/assets/vendor/echarts/echarts.min.js"></script>
-  <script src="<?=ROOT?>/assets/vendor/quill/quill.min.js"></script>
-  <script src="<?=ROOT?>/assets/vendor/simple-datatables/simple-datatables.js"></script>
-  <script src="<?=ROOT?>/assets/vendor/tinymce/tinymce.min.js"></script>
-  <script src="<?=ROOT?>/assets/vendor/php-email-form/validate.js"></script>
 
   <!-- Template Main JS File -->
   <script src="<?=ROOT?>/assets/js/main.js"></script>
