bc0a699aa7e441d5dc7225ff36486da6f750ddb9
cyrilmuchemi <cyril.muchemi@gmail.com>
2023-11-10 13:20:02 +0300

    Make the sliders dynamic && start working on Homepage

--- app/pages/includes/slider.php
+++ app/pages/includes/slider.php
@@ -1,20 +1,20 @@
-<!--slider -->
 <link rel="stylesheet" href="<?=ROOT?>/assets/slider/ism/css/my-slider.css"/>
-        <script src="<?=ROOT?>/assets/slider/ism/js/ism-2.2.min.js"></script>
-        <div class="ism-slider" data-transition_type="fade" data-play_type="loop" id="my-slider">
-        <ol>
+<script src="<?=ROOT?>/assets/slider/ism/js/ism-2.2.min.js"></script>
+
+<?php
+$limit = 10; 
+$offset = ($PAGE['page_number'] - 1) * $limit;
+$query = "SELECT * FROM slides ORDER BY id DESC LIMIT $limit OFFSET $offset";
+$rows = query($query);
+?>
+
+<div class="ism-slider" data-transition_type="fade" data-play_type="loop" id="my-slider">
+    <ol>
+        <?php foreach ($rows as $row) : ?>
             <li>
-            <img src="<?=ROOT?>/assets/slider/ism/image/slides/dstv.png">
+                <img src="<?=get_image($row['image'])?>">
+                <p class="ism-caption"><?=$row['caption']?></p>
             </li>
-            <li>
-            <img src="<?=ROOT?>/assets/slider/ism/image/slides/verbit.png">
-            </li>
-            <li>
-            <img src="<?=ROOT?>/assets/slider/ism/image/slides/writerbay.png">
-            </li>
-            <li>
-            <img src="<?=ROOT?>/assets/slider/ism/image/slides/netflix.png">
-            </li>
-        </ol>
-        </div>
-    <!--end slider -->
+        <?php endforeach; ?>
+    </ol>
+</div>
